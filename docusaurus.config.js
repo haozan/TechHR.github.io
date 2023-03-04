dd46ab17818d012f5c5d7a217777cc9d65553c7d
haozan <26126632+haozan@users.noreply.github.com>
2023-03-04 16:01:31 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: '技术招聘',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
