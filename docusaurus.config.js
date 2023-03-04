12024b41aabf57d68ccd551bcd9f0584cacc438c
haozan <26126632+haozan@users.noreply.github.com>
2023-03-04 17:15:11 +0800

    Revert "Update docusaurus.config.js"
    This reverts commit dd46ab17818d012f5c5d7a217777cc9d65553c7d.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '技术招聘',
+  title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
